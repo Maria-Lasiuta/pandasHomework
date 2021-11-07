fcf0fd0066be879aab61ba762f030e50e2a1cacc
OlhaVeselovska <90180548+OlhaVeselovska@users.noreply.github.com>
2021-11-07 18:22:37 +0200

    Pandas(Teams)

--- Teams.py
+++ Teams.py
@@ -1,62 +1,28 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[9]:
-
 
 import pandas as pd
 
-
-# In[15]:
-
-
 teams = pd.read_csv('teams.csv',sep=',')
-
-
-# In[23]:
 
 
 teams.head(16)
 
 
-# ### Who was a winner?
-
-# In[40]:
-
+#1  Who was a winner?
 
 teams.Team.loc[(teams.Goals == teams.Goals.max())]
 
 
-# ### How many red cards there were and which team got it?
-
-# In[75]:
+#2 How many red cards there were and which team got it?
 
 
 teams['Red Cards'].sum()
 
 
-# In[76]:
-
-
 teams[teams['Red Cards'] == 1].Team
 
 
-# ### Which teams have shooting accurancy more than 50 % and saves-to-shots ratio are more than 80%?
-
-# In[53]:
-
+#3 Which teams have shooting accurancy more than 50 % and saves-to-shots ratio are more than 80%?
 
 teams[(teams['Shooting Accuracy'] > '50%') & (teams['Saves-to-shots ratio'] > '80%')]
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
